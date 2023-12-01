b1adbcd14406f7ac4ad25e4f98a653329da60585
pizza6inch <pizza6inch@gmail.com>
2023-12-01 18:11:36 +0800

    APItestComplete

--- Backend/api.py
+++ Backend/api.py
@@ -1,23 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+import mysql as sql
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# 假設有一個用戶資訊
-user_info = {"name": "John Doe", "age": 30}
 
-# GET 請求的端點
 @app.route('/api/user', methods=['GET'])
 def get_user_info():
-    return jsonify(user_info)
+    return jsonify("get")
 
-# POST 請求的端點
 @app.route('/api/user', methods=['POST'])
 def submit_user_info():
-    data = request.get_json()
-    user_info["name"] = data["name"]
-    user_info["age"] = data["age"]
-    print("user_info : ", user_info)
-    return jsonify(user_info)
+    sql.pr()
+    sql.manageCommand(request.get_json())
+    return jsonify("success")
+
+'''@app.route('/api/user')
+def renderHtml():
+    return render_template('home.html')'''
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/mysql.py
+++ Backend/mysql.py
@@ -1,28 +1,43 @@
 import pymysql
+import json
 # 資料庫參數設定
 db_settings = {
     "host": "127.0.0.1",
     "port": 3306,
     "user": "root",
-    "password": "1236987450",
-    "db": "kkbox",
+    "password": "qq816816", # 記得更改密碼
+    "db": "mydb",
     "charset": "utf8"
 }
-try:
-    # 建立Connection物件
-    conn = pymysql.connect(**db_settings)
-    if conn != None:
-        print("yes")
-    # 建立Cursor物件
-    with conn.cursor() as cursor:
-        '''command = "INSERT INTO charts(id, name, artists)VALUES(2,5,8)"
-        cursor.execute(command)'''
-        command = "SELECT * FROM charts"
-        # 執行指令
-        cursor.execute(command)
-        # 取得所有資料
-        result = cursor.fetchall()
-        print(result)
-        #conn.commit()
-except Exception as ex:
-    print(ex)
+
+def pr():
+    print("1")
+
+def insert(dst:str, *args):
+    return f"INSERT INTO {dst} VALUES{args}"
+def countTable(table:str):
+    if command(f"Select Count(*) From {table}") == None:
+        return 0
+    return command(f"Select Count(*) From {table}")[0][0]
+
+def manageCommand(data):
+    print(data)
+    if data['type'] == "login":
+        id = countTable("user")
+        command(insert("user",id+1,data['username'],data['password']))
+    
+def command(waitting_command):
+    try:
+        conn = pymysql.connect(**db_settings)
+        if conn == None:
+            print('fail')
+            return
+        with conn.cursor() as cursor:
+            cursor.execute(waitting_command)
+            result = cursor.fetchall()
+            print(result)
+            conn.commit()
+            return result
+    except Exception as ex:
+        print(ex)
+    return None
