c336cbe1cc18346e3a3cbaae3fde59da8672f341
edward <edwall201@gmail.com>
2023-12-03 00:45:15 +0800

    feat:新增LoginPage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
-import MainPage from "./Components/TopNav";
+// import MainPage from "./Components/TopNav";
+import LoginPage from "./Pages/LoginPage";
 
 export default function App() {
   return (
     <div>
       <Routes>
       <Route path="/">
-        <Route index element={<MainPage/>}/>
+       <Route index element={<LoginPage/>}/>
+       {/* <Route index element={<MainPage/>}/> */}
+       <Route index element={<LoginPage/>}/>
       </Route>
       </Routes>
     </div>
---
+++ Frontend/src/Pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TopNav from "../Components/MemberLogin"
+
+export default function LoginPage() 
+{
+	return(
+		<>
+			<TopNav></TopNav>
+		</>
+	)
+}
