6085e1bd427efc10ca0b79efd2526f993da948c7
Madfater <ericleeddd@gmail.com>
2023-12-03 06:33:32 +0800

    fix: 改為正確的結構

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 // import MainPage from "./Components/TopNav";
 import LoginPage from "./Pages/LoginPage";
+import MainPage from "./Components/TopNav";
 
 export default function App() {
   return (
     <div>
       <Routes>
       <Route path="/">
-       <Route index element={<LoginPage/>}/>
+       <Route index element={<MainPage/>}/>
        {/* <Route index element={<MainPage/>}/> */}
-       <Route index element={<LoginPage/>}/>
+       <Route path="/login" element={<LoginPage/>}/>
       </Route>
       </Routes>
     </div>
