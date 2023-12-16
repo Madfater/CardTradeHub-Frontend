ad71c635de6f20782c410d6132ca470ac90be93f
Madfater <ericleeddd@gmail.com>
2023-12-16 20:47:32 +0800

    fix : merge problem

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 // import MainPage from "./Components/TopNav";
 import LoginPage from "./Pages/LoginPage";
-import MainPage from "./Components/TopNav";
+import MainPage from "./Pages/MainPage";
 
 export default function App() {
   return (
     <div>
       <Routes>
-      <Route path="/">
-       <Route index element={<MainPage/>}/>
-       {/* <Route index element={<MainPage/>}/> */}
-       <Route path="/login" element={<LoginPage/>}/>
-      </Route>
+        <Route path="/">
+          <Route index element={<MainPage />} />
+          {/* <Route index element={<MainPage/>}/> */}
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
       </Routes>
     </div>
   );
