a5a91d642886cd0298e8906766149a9a59957cc6
Madfater <ericleeddd@gmail.com>
2023-12-16 17:49:20 +0800

    del: 刪除pycache

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
