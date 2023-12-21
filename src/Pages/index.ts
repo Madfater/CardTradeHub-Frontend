785c8e26e969089f897ed1d2b9187e94ff87ab6d
Madfater <ericleeddd@gmail.com>
2023-12-21 17:22:15 +0800

    style: 修改頁面匯出方式

---
+++ src/Pages/index.ts
@@ -0,0 +1,6 @@
+export { default as LoginPage } from './LoginPage';
+export { default as MainPage } from './MainPage';
+export { default as ShoppingCart } from './ShoppingCart';
+export { default as StoreManager } from './StoreManager';
+export { default as SearchPage } from './SearchPage';
+export { default as RegisterPage } from './RegisterPage';
