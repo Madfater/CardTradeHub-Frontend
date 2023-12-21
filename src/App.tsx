f414cd0657e5e214bdf7d6ad5c76aae258144061
Madfater <ericleeddd@gmail.com>
2023-12-21 17:20:32 +0800

    feat: 新增註冊頁面

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import LoginPage from "./Pages/LoginPage";
-import MainPage from "./Pages/MainPage";
-import ShoppingCart from "./Pages/ShoppingCart";
-import StoreManager from "./Pages/StoreManager"
-import SearchPage from "./Pages/SearchPage";
+import { Routes, Route } from "react-router-dom";
+import {
+  LoginPage,
+  MainPage,
+  ShoppingCart,
+  StoreManager,
+  SearchPage,
+  RegisterPage,
+} from "./Pages";
+import { AuthProvider } from "./Contexts/AuthContext";
 
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/">
-          <Route index element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/shoppingcart" element={<ShoppingCart />} />
-          <Route path="/storemanager" element={<StoreManager/>}/>
-          <Route path="/searchpage" element={<SearchPage />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/">
+            <Route index element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/shoppingcart" element={<ShoppingCart />} />
+            <Route path="/storemanager" element={<StoreManager />} />
+            <Route path="/searchpage" element={<SearchPage />} />
+            <Route path="/registerpage" element={<RegisterPage/>}/>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
