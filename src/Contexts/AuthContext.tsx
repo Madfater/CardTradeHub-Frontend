41aa9298cf9744f6c8387764d929726a024048b9
Madfater <ericleeddd@gmail.com>
2023-12-21 17:22:40 +0800

    feat: 新增用戶Context

---
+++ src/Contexts/AuthContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface AuthContextType {
+  userId: string | null;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [userId, setUserId] = useState<string | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ userId }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
